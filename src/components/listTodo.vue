fdf727524d3073242547bacdc516178b6f81adde
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2023-04-24 10:16:03 +0200

    999

--- src/components/listTodo.vue
+++ src/components/listTodo.vue
@@ -4,11 +4,11 @@
 
 
     
-    <div v-for="todo in listTodo" :key="id" class="card-body border w-50 p-3">
+    <div v-for="todo in listTodo" :key="id" class="card-body border p-3">
         <div>
-          <input class="toggle m-3" type="checkbox" v-model="todo.completed" @change="editTodo(todo)" />
-          <input type="text" @blur="editTodo(todo)" autofocus v-if="todo.completed" class="card-text shadow p-3 mb-5 bg-body-tertiary rounded text-decoration-line-through" v-model="todo.todo" />
-          <input type="text" @blur="editTodo(todo)" autofocus v-else class="card-text shadow p-3 mb-5 bg-body-tertiary rounded" v-model="todo.todo" />
+          <input class="toggle m-3 " type="checkbox" v-model="todo.completed" @change="editTodo(todo)" />
+          <input type="text" @blur="editTodo(todo)" autofocus v-if="todo.completed" class="w-75 card-text shadow p-3 mb-5 bg-body-tertiary rounded text-decoration-line-through" v-model="todo.todo" />
+          <input type="text" @blur="editTodo(todo)" autofocus v-else class="w-75 card-text shadow p-3 mb-5 bg-body-tertiary rounded" v-model="todo.todo" />
           <button class="btn btn-danger mx-3 p-3" @click="deleteTodo(todo.id)">Delete</button>
 
         </div>
