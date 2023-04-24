4001aef58368c21b4b11077d957ae14b24aac854
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2023-04-24 09:37:13 +0200

    Funciona Patch

--- src/components/listTodo.vue
+++ src/components/listTodo.vue
@@ -1,25 +1,26 @@
 <template>
+  <div class="row container-fluid" id="CardContainer">
+    <div class="card border">
 
-    <div class="row container-fluid" id="CardContainer">
-<div class="card border">
-    
-    <div v-for="todo in listTodo" :key="id" class="card-body border">
+      <div v-for="todo in listTodo" :key="id" class="card-body border">
         <div>
-          <input class="toggle" type="checkbox" v-model="todo.completed" />
-          <input type="text" @blur="editTodo(todo.todo)" autofocus v-if="todo.completed" class="card-text text-decoration-line-through" v-model="todo.todo" />
-          <input type="text" @blur="editTodo(todo.todo)" autofocus v-else class="card-text" v-model="todo.todo" />
+          <input class="toggle" type="checkbox" v-model="todo.completed" @change="editTodo(todo)"/>
+          <input type="text" @blur="editTodo(todo)" autofocus v-if="todo.completed"
+            class="card-text text-decoration-line-through" v-model="todo.todo" />
+          <input type="text" @blur="editTodo(todo)" autofocus v-else class="card-text" v-model="todo.todo" />
 
         </div>
-        <button>Editar</button>
+      
         <button @click="deleteTodo(todo.id)">Delete</button>
+      </div>
     </div>
-</div>
-    </div>
+  </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
-import  axios  from "axios";
+import axios from "axios";
+import { computeStyles } from "@popperjs/core";
 const listTodo = ref([]);
 
 
@@ -34,10 +35,26 @@ const getName = async () => {
   }
 }
 getName()
-async function deleteTodo(id){
+async function deleteTodo(id) {
   try {
     const response = await axios.delete(`http://localhost:3000/todos/${id}`)
-    listTodo.value = listTodo.value.filter((todo) => todo.id !==id)
+    listTodo.value = listTodo.value.filter((todo) => todo.id !== id)
+    //Aprovecho la reactividad de listTodo para actualizar la vista
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function editTodo(todo) {
+  console.log(todo);
+ 
+
+  try {
+    const response = await axios.patch(`http://localhost:3000/todos/${todo.id}`, {
+    "todo": todo.todo,
+    "completed": todo.completed
+  })
+
     //Aprovecho la reactividad de listTodo para actualizar la vista
   } catch (error) {
     console.log(error)
@@ -45,6 +62,4 @@ async function deleteTodo(id){
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
